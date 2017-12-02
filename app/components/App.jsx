9b9eabfd5175d88fd81637535227742237acdce9
Pablo Sánchez <pablosygmail@.com>
2017-12-02 14:18:45 +0100

    First Changes

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import './../assets/scss/main.scss';
+import { visits } from "./../assets/mock-data";
+import VisitList from "./VisitList";
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <h2 id="heading">Hello ReactJS</h2>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            visits: visits,
+        };
+    }
+    render() {
+        return (
+            <div>
+                <VisitList visits={visits}/>
+            </div>
+        );
+    }
 
 }
 
