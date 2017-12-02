4ba12581ba379467c5245745d349f03111f6c9a0
Pablo Sánchez <pablosygmail@.com>
2017-12-02 18:29:43 +0100

    Bootstrap Added, Prototype made

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import './../assets/scss/main.scss';
 import { visits } from "./../assets/mock-data";
 import VisitList from "./VisitList";
+import {PageHeader} from 'react-bootstrap'
+import {Grid} from 'react-bootstrap';
+import {Row} from 'react-bootstrap';
+import {Col} from 'react-bootstrap';
+
 
 export default class App extends React.Component {
     constructor(props) {
@@ -12,9 +17,15 @@ export default class App extends React.Component {
     }
     render() {
         return (
-            <div>
-                <VisitList visits={visits}/>
-            </div>
+            <Grid>
+                <PageHeader>Customer Relationship Management <small>CRM</small></PageHeader>
+            <Row>
+                <Col xs={12} md={8}>
+                    <h1>Lista de visitas</h1>
+                    <VisitList visits={visits}/>
+                </Col>
+            </Row>
+            </Grid>
         );
     }
 
--- app/components/VisitList.jsx
+++ app/components/VisitList.jsx
@@ -1,10 +1,40 @@
 import React from 'react';
+import { ListGroup } from 'react-bootstrap';
+import {ListGroupItem} from 'react-bootstrap';
+import {PanelGroup} from 'react-bootstrap'
+import {Panel} from 'react-bootstrap';
 
 export default class VisitList extends React.Component {
     render() {
-        console.log(this.props.c);
+        console.log(this.props.visits);
         return (
-            <ul>Aquí va la lista de visitas:</ul>
+                <PanelGroup>
+                    { this.props.visits.map((visit, index) => {
+                        return (
+                                <Panel collapsible defaultExpanded header={visit.Customer.name} bsStyle="primary" key={index}>
+                                    <ListGroup fill>
+                                        <ListGroupItem header="Vendedor">{visit.Salesman.fullname}</ListGroupItem>
+                                        <ListGroupItem header="Fecha planeada">{visit.plannedFor}</ListGroupItem>
+                                        <ListGroupItem header="Dirección">{visit.Customer.address1}</ListGroupItem>
+                                        <ListGroupItem header="Teléfono">{visit.Customer.phone1}</ListGroupItem>
+                                        <ListGroupItem header="Targets" bsStyle="info">
+                                        {visit.Targets.map((target, tIndex) => {
+                                            return (
+                                                <ListGroup key={tIndex}>
+                                                    <ListGroupItem header="Notas">{target.notes}</ListGroupItem>
+                                                    <ListGroupItem>{target.Company.name}</ListGroupItem>
+                                                    <ListGroupItem>{target.Company.web1}</ListGroupItem>
+                                                </ListGroup>
+                                            );
+                                        }
+                                        )}
+                                        </ListGroupItem>
+                                    </ListGroup>
+                                </Panel>
+                        );
+                    })
+                    }
+                </PanelGroup>
         );
     }
 }
